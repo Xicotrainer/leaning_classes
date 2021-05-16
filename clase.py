91a39c92368503eaf393f66a96e22848374757a3
Xicoténcatl Pérez López <Ecbalam_4to@hotmail.com>
2021-05-16 13:21:35 -0500

    (1-B-5) Rescritura del contructor para la clase deposrtista. Corrección en la identación en los diccionarios que fungen como parametros del método hablar. Corrección de la instancia Deposrtista

--- clase.py
+++ clase.py
@@ -15,6 +15,15 @@ class Persona:
 
 class Deportista(Persona):
 
+	# Sobreescritura del constructor 
+	def __init__(self, edad, nombre, deporte):
+		self.edad = edad
+		self.nombre = nombre
+		self.deporte = deporte
+
+		print("Se ha creado a {} con {} años de edad practicante de {}. \n".
+			format(self.nombre, self.edad, self.deporte))
+
 	def practicarDeporte(self):
 		print("{}: voy a practicar\n".
 				format(self.nombre))
@@ -24,15 +33,18 @@ class Deportista(Persona):
 Aragorn = Persona(37, "Aragorn")
 Boromir = Persona(36, "Boromir")
 
-Aragorn.hablar( f1 = "Voy a ser el prota en la parte III.", 
-				f2 = "Me guta, hmmm jmm.")
-Boromir.hablar( f1 = "A mi me carga chandingas en la peli I.", 
-				f2 = "Oh rayios!!!")
+Aragorn.hablar(
+	f1 = "Voy a ser el prota en la parte III.", 
+	f2 = "Me guta, hmmm jmm.")
+Boromir.hablar(
+	f1 = "A mi me carga chandingas en la peli I.", 
+	f2 = "Oh rayios!!!")
 
-# Legolas es un elfo.. ¿es una persosna?
-Legolas = Deportista(2931, "Legolas")
-Legolas.practicarDeporte()
+# Instancia de la nueva clase
+Michael_Phelps = Deportista(35,  "Michael", "Natación")
+Michael_Phelps.practicarDeporte()
 
-Legolas.hablar( f1 = "No como Gimli",
-				f2 = "Él es peligroso en distancias cortas", 
-				f3 = "jajajjaja")
+Michael_Phelps.hablar(
+	f1 = "Me llaman la bala de Baltimor y no me gusta mi segundo nombre, Fred",
+	f2 = "Debute en el 2000 y con un total de 28 medallas en mi carrera profesional",
+	f3 = "me retiré en el 2016.")
