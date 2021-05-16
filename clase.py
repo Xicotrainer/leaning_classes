0e2dd61e81cb3f5e51539a418add13ecc0d75f33
Xicoténcatl Pérez López <Ecbalam_4to@hotmail.com>
2021-05-16 12:47:42 -0500

    (1-B-3) Se arega una clase deportista que hereda propiedades de persona y se efectua la identación correspondiente. Se colocan los parametro de forma vertical para el diccionario en el método hablar.

--- clase.py
+++ clase.py
@@ -1,4 +1,5 @@
 class Persona:
+	
 	def __init__(self, edad, nombre):
 		self.edad = edad
 		self.nombre = nombre
@@ -11,8 +12,27 @@ class Persona:
 			print ("{}: {} \n".
 				format(self.nombre, palabras[frase]))
 
-# Instancia
+
+class Deportista(Persona):
+
+	def practicarDeporte(self):
+		print("{}: voy a practicar\n".
+				format(self.nombre))
+
+
+# Instancias
 Aragorn = Persona(37, "Aragorn")
 Boromir = Persona(36, "Boromir")
-Aragorn.hablar(f1 = "Voy a ser el prota en la parte III.", f2 = "Me guta, hmmm jmm.")
-Boromir.hablar(f1 = "A mi me carga chandingas en la I.", f2 = "Oh rayios!!!")
+
+Aragorn.hablar( f1 = "Voy a ser el prota en la parte III.", 
+				f2 = "Me guta, hmmm jmm.")
+Boromir.hablar( f1 = "A mi me carga chandingas en la peli I.", 
+				f2 = "Oh rayios!!!")
+
+# Legolas es un elfo.. ¿es una persosna?
+Legolas = Deportista(2931, "Legolas")
+Legolas.practicarDeporte()
+
+Legolas.hablar( f1 = "No como Gimli",
+				f2 = "Él es peligroso en distancias cortas", 
+				f3 = "jajajjaja")
