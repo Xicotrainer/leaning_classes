b572f12047ed874984983b1ad49bfd5dc75db053
Xicoténcatl López <81500424+Xicotrainer@users.noreply.github.com>
2021-05-16 12:52:50 -0500

    Método hablar: Identación en los diccionario
    La identación en los diccionarios que fungen de parámetros esta dispareja.

--- clase.py
+++ clase.py
@@ -25,14 +25,14 @@ Aragorn = Persona(37, "Aragorn")
 Boromir = Persona(36, "Boromir")
 
 Aragorn.hablar( f1 = "Voy a ser el prota en la parte III.", 
-				f2 = "Me guta, hmmm jmm.")
+		f2 = "Me guta, hmmm jmm.")
 Boromir.hablar( f1 = "A mi me carga chandingas en la peli I.", 
-				f2 = "Oh rayios!!!")
+		f2 = "Oh rayios!!!")
 
 # Legolas es un elfo.. ¿es una persosna?
 Legolas = Deportista(2931, "Legolas")
 Legolas.practicarDeporte()
 
 Legolas.hablar( f1 = "No como Gimli",
-				f2 = "Él es peligroso en distancias cortas", 
-				f3 = "jajajjaja")
+		f2 = "Él es peligroso en distancias cortas", 
+		f3 = "jajajjaja")
