68fc043259e78784127807fb3866588e4f8f362d
Xicotrainer <xicotrainer@gmail.com>
2021-05-18 23:28:37 -0500

    'Table' is a SQL reserved word. This scripts runs pretty well the first time then show an error when It trys to create the same BBDD.bd file

--- Tables/pracdb.py
+++ Tables/pracdb.py
@@ -5,7 +5,17 @@ APP_path = os.getcwd()
 DB_path = APP_path + "/base.db"
 
 con = sqlite3.connect(DB_path)
+cursor = con.cursor()
+
+cursor.execute(
+	"""
+	CREATE TABLE users_(
+		ID		INT		PRIMARY KEY		NOT NULL,
+		name	TEXT 					NOT NULL,
+		age		INT,
+		status 	BIT 					NOT NULL
+		)
+	"""
+	)
+
 con.close()
-
-
-
